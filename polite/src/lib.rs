c0725b638ac0b5090b6d8fa518e26aba49429c54
crumplecup <erik.w.rose@gmail.com>
2024-04-26 17:19:19 -0700

    fix: nom errors

--- polite/src/lib.rs
+++ polite/src/lib.rs
@@ -1,3 +1,7 @@
 pub mod polite;
 
-pub use crate::polite::{FauxPas, Polite};
+pub use polite::{FauxPas, Polite};
+
+#[cfg(feature = "parse")]
+#[cfg_attr(docsrs, doc(cfg(feature = "parse")))]
+pub use polite::NomDescript;
