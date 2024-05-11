9b6d627e8781f4fa7027eb7298e6853b820e98fe
crumplecup <erik.w.rose@gmail.com>
2024-05-10 19:34:33 -0700

    rollback winit for egui

--- polite/src/polite.rs
+++ polite/src/polite.rs
@@ -1,9 +1,9 @@
 //! The `polite` crate defines a [`FauxPas`] alias for [`Error`], and an alias for
 //! [`Result`], [`Polite`], using the [`Error`] alias.
-use thiserror::Error;
 #[cfg(feature = "parse")]
 #[cfg_attr(docsrs, doc(cfg(feature = "parse")))]
 use std::fmt;
+use thiserror::Error;
 
 /// The `Polite` type is an alias for `Result` using the library-defined [`FauxPas`].
 pub type Polite<T> = Result<T, FauxPas>;
